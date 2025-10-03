28f417f710dfd8befc59163b5a4c37ce54b26142
nguyensontung2304 <nguyensontungnst2304@gmail.com>
2025-10-03 17:45:20 +0700

    thay đổi style

--- src/pages/blogDetail/BlogDetail.jsx
+++ src/pages/blogDetail/BlogDetail.jsx
@@ -1,47 +1,9 @@
-// import { useParams } from "react-router-dom";
-// import { BLOG_DATA } from "../../constants/blogData";
-// import "./BlogDetail.css";
-// import React, { useEffect, useState } from "react";
-// import { addViewedPost } from "../watchedpost/WatchedPost";
-
-// function BlogDetail() {
-//   const params = useParams();
-//   const [state, setState] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(false);
-
-//   useEffect(() => {
-//     fetch(
-//       `https://67e105a058cc6bf78523e2e1.mockapi.io/api/v1/post/${params.id}`
-//     )
-//       .then((res) => res.json())
-//       .then((json) => setState(json))
-//       .then(() => setLoading(false))
-//       .catch(() => setError(true));
-//   }, []);
-
-//   // const data = BLOG_DATA.find((item) => item.slug === params.slug);
-
-//   // useEffect(() => {
-//   //   if (params.slug) {
-//   //     addViewedPost(params.slug);
-//   //   }
-//   // }, []);
-
-//   return (
-//     <div className="detail">
-//       <div className="detail_title">{data.title}</div>
-//       <div className="detail_content">{data.content}</div>
-//     </div>
-//   );
-// }
-
-// export default BlogDetail;
-
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import Related from "./Related/Related";
 import { addViewedPost } from "../watchedpost/WatchedPost";
 import "./BlogDetail.css";
+import Loading from "../../components/Loading/Loading";
 
 function BlogDetail() {
   const params = useParams();
@@ -51,8 +13,6 @@ function BlogDetail() {
 
   useEffect(() => {
     if (!params.id) return;
-
-    setLoading(true);
 
     fetch(
       `https://67e105a058cc6bf78523e2e1.mockapi.io/api/v1/post/${params.id}`
@@ -68,16 +28,36 @@ function BlogDetail() {
     addViewedPost(params.id);
   }, [params.id]);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <Loading />;
+
   if (error) return <div>Error...</div>;
+
   if (!data) return null;
 
   return (
     <div className="detail">
       <div className="detail_title">{data.title}</div>
       <div className="detail_content">{data.content}</div>
+      <Related type={data.type} />
     </div>
   );
 }
 
 export default BlogDetail;
+
+// vòng đời component khi chạy
+// giai đoạn initialization sẽ khởi tạo
+// const params = useParams();
+// const [data, setData] = useState(null);
+// const [loading, setLoading] = useState(true);
+// const [error, setError] = useState(false);
+// và rơi vào
+// if (loading) return <Loading />;
+// if (error) return <div>Error...</div>;
+// if (!data) return null;
+// xong qua giai đoạn mounting để render ra dom
+// thì useEffect chạy khi useEffect chạy xong thì có data
+// và loading false thì có sự thay đổi của state nên component render lại
+// thì bắt đầu vào giai đoạn updation rơi vào nhánh đã có data
+// và loading false thì nó không chạy 3 dòng if nữa nó chạy xuống return trong return có Related thì
+// component Related chạy vòng đời của riêng nó
